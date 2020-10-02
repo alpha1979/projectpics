7d01fe95c9378cd969275d440b0cdbf72852cf3c
alpha1979 <atit1979@hotmail.com>
2020-10-03 00:20:57 +0100

    Updates

---
+++ precache-manifest.b3a31c5f91ca8d1873623000323a121b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d8afedd53e3885cc9a059a8593c055",
+    "url": "/projectpics/index.html"
+  },
+  {
+    "revision": "cbdcd54db6c2e59e779c",
+    "url": "/projectpics/static/js/2.5ded9f06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projectpics/static/js/2.5ded9f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a268aeeb4fb73a1e12",
+    "url": "/projectpics/static/js/main.366cbb5b.chunk.js"
+  },
+  {
+    "revision": "97eeb78f7f5be84a27eb",
+    "url": "/projectpics/static/js/runtime-main.42d02331.js"
+  }
+]);
